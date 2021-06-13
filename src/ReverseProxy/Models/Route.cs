39136f540500310725c7ea77f7f876faa9235925
Alexander Hammatt <alexander.hammatt@protonmail.com>
2021-06-13 17:50:43 +1200

    add a todo note so i remember what i was in the middle of

---
+++ src/ReverseProxy/Models/Route.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ReverseProxy.Models
+{
+    public class Route
+    {
+        /*
+         * TODO: create a route model for the destination service to match up against
+         * needs to have at least two different types of route, static content and reverse proxy
+         * static content is mostly for testing, but other reverse proxies also support this kind of thing.
+         */
+    }
+}
